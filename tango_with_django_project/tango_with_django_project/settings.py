508c965d36a526cc4a312d8d44995b83944a4d44
pmh118 <1104636h@student.gla.ac.uk>
2017-01-19 17:34:07 +0000

    Chapter 4 completed image added to webpage

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,11 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(__file__)
-print(os.path.dirname(__file__))
-print(os.path.dirname(os.path.dirname(__file__)))
-
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates') #creates a dynamic path to templates folder
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -30,6 +28,14 @@ SECRET_KEY = 'l)h8c$c06v2c5b2$o%fqim8mdb!g9iz^5246o%%vn3&=)qr$oq'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
+
+#Some Variables added
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
 
 
 # Application definition
@@ -68,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -126,3 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
