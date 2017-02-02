7887762811f2656654df6169fdb939cb444f4715
Paul Hall <1104636h@student.gla.ac.uk>
2017-02-02 10:45:36 +0000

    chapter 3 ammended with about link back to home

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^rango/about', views.about, name='about'),
+    url(r'^about/index', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
